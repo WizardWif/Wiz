79c49988bac94e83a02c25529022957f9fe80ff6
WizardWif <spinwizard2024@gmail.com>
2024-10-02 10:07:22 +0200

    Update TradeBuzzySection.jsx

--- src/components/Home/TradeBuzzySection.jsx
+++ src/components/Home/TradeBuzzySection.jsx
@@ -11,11 +11,11 @@ export default function TradeBuzzySection() {
         </h1>
         <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 md:gap-20 gap-10 w-full mt-14 p-0">
           <div className="token flex justify-center items-center">
-            <a href="https://dexscreener.com/solana/hrazc2arvekhdgpevmvbqhxkvbgfnllxvyndg1g4t5dd" target="_blank" rel="noopener noreferrer">
+            <a href="https://dexscreener.com/solana/cgq44kxxi4vek668tfonkdb6kuyjmxbhapsjhmwysst3" target="_blank" rel="noopener noreferrer">
             <img src="/images/logo1.png" className="w-full" alt="" /></a>
           </div>
           <div className="token flex justify-center items-center">
-            <a href="https://dexscreener.com/solana/hrazc2arvekhdgpevmvbqhxkvbgfnllxvyndg1g4t5dd" target="_blank" rel="noopener noreferrer">
+            <a href="https://dexscreener.com/solana/cgq44kxxi4vek668tfonkdb6kuyjmxbhapsjhmwysst3" target="_blank" rel="noopener noreferrer">
             <img src="/images/logo2.jpeg" className="w-full  " alt="" /></a>
           </div>
           <div className="token flex justify-center items-center">
@@ -26,7 +26,7 @@ export default function TradeBuzzySection() {
         <div className="flex justify-center  translate-y-12">
           <HexaGoan customCss={"w-[220px]"}>
             <h1 className="text-4xl text-center font-bold leading-10">
-              <a href="https://dexscreener.com/solana/hrazc2arvekhdgpevmvbqhxkvbgfnllxvyndg1g4t5dd" target="_blank">
+              <a href="https://dexscreener.com/solana/cgq44kxxi4vek668tfonkdb6kuyjmxbhapsjhmwysst3" target="_blank">
                 BUY $BUZZY NOW
               </a>
             </h1>
